79305dfeb5d2a047547b23cbdbb85da07c0a6460
TenNM <pacs3131@gmail.com>
2020-09-12 22:04:24 +0300

    Добавьте файлы проекта.

---
+++ FileIOSerializer.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace sandboxCS
+{
+    internal abstract class FileIOSerializer
+    {
+        internal const string SAVE_ERR = "Save error";
+        internal const string LOAD_ERR = "Load error";
+        //------------------------------------------------binary serialization, save, load
+        internal static void save(object saveableObj, string path)
+        {
+            var binFormatter = new BinaryFormatter();
+            using (var fStream = new FileStream(path, FileMode.Create))
+            {
+                binFormatter.Serialize(fStream, saveableObj);
+            }
+        }
+        internal static void save(object saveableObj, Stream fs)
+        {
+            var binFormatter = new BinaryFormatter();
+            try
+            {
+                binFormatter.Serialize(fs, saveableObj);
+            }
+            catch { throw new System.Exception(SAVE_ERR); }
+        }
+        //--------------------------------------------------------------------------
+        internal static void load<T>(ref T loadableObj, string path) where T : class
+        {
+            var binFormatter = new BinaryFormatter();
+            using (var fStream = new FileStream(path, FileMode.Open))
+            {
+                loadableObj = binFormatter.Deserialize(fStream) as T;
+            }
+        }
+        internal static void load<T>(ref T loadableObj, Stream fs) where T : class
+        {
+            var binFormatter = new BinaryFormatter();
+            try
+            {
+                loadableObj = binFormatter.Deserialize(fs) as T;
+            }
+            catch { throw new System.Exception(LOAD_ERR); }
+        }
+        //---------------------------------------------------------------------------
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,37 @@
+﻿namespace sandboxCS
+{
+    internal abstract class Service
+    {
+        //----------------------------------------------------------------------
+        internal static bool stringIsNumber(string s)
+        {
+            bool weHaveOneComma = false;
+            if (s[0].Equals(',')) { weHaveOneComma = true; }
+            else if (!(char.IsDigit(s[0]) || s[0].Equals('-'))) { return false; }
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i].Equals(','))
+                {
+                    if (weHaveOneComma) { return false; }
+                    else weHaveOneComma = true;
+                }
+                else if (!char.IsDigit(s[i])) { return false; }
+            }
+
+            return true;
+        }
+
+        internal static string dotsToCommas(string withCommasStr)
+        {
+            string tempStr = "";
+            foreach (char c in withCommasStr)
+            {
+                if ('.' == c) { tempStr += ','; }
+                else tempStr += c;
+            }
+            return tempStr;
+        }
+        //------------------------------------------------------------------------------end
+    }
+}
